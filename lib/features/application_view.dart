a3645f0f66a5f7054fdd63b0ffcf01a333d71699
Nicolas HAHN <nicolas.hahn@unilor.fr>
2019-09-09 15:16:33 +0200

    Changed bottom sheet bar theme

--- lib/features/application_view.dart
+++ lib/features/application_view.dart
@@ -12,19 +12,20 @@ class ApplicationView extends StatelessWidget {
 
     return MaterialApp(
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        backgroundColor: Colors.white,
-        canvasColor: Colors.transparent
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          backgroundColor: Colors.white,
+          bottomSheetTheme: Theme.of(context)
+              .bottomSheetTheme
+              .copyWith(backgroundColor: Colors.transparent)),
       navigatorKey: navigationService.navigatorKey,
       initialRoute: Routes.home,
       onGenerateRoute: Router.generateRoutes,
